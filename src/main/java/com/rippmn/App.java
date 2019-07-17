208f633cb7f261a238132cad6ac55263e668fad4
rippmn <rippmn@gmail.com>
2019-07-17 14:10:46 -0500

    made print 4 boards

--- src/main/java/com/rippmn/App.java
+++ src/main/java/com/rippmn/App.java
@@ -8,9 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        for (int i = 8; i < 16; i++) {
+
+        for (int i = 256; i <= 511; i++) {
             BracketNode bn = new BracketNode(i);
-            System.out.println(bn.getInsertValue());
+            if(i%64==0){
+                System.out.println("\n------------------\n");
+            }
+            System.out.println((bn.getInsertValue()+1));
         }
     }
 }
